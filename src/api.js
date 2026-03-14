a4b4f2f496b0dc15a99ebccb304a402871182405
wolf399 <eldeebfares762@gmail.com>
2026-03-14 20:35:48 +0200

    Use env var for API URL

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api';
+
 
 const getToken = () => localStorage.getItem('token');
 
